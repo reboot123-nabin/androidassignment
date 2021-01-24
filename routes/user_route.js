cdd8272e56641c0ff2ffffaf9d7bdcfe36c34e65
unknown <reboot123-nabin>
2021-01-24 10:40:39 +0545

    login

--- routes/user_route.js
+++ routes/user_route.js
@@ -5,7 +5,8 @@ const router=express.Router();
 //for validation our user data
 const{check,validationResult}=require('express-validator');
 //for password descryption
-const bcryptjs=require('bcryptjs')
+const bcryptjs=require('bcryptjs');
+const { find } = require('../modules/user_model');
 
 router.post('/user/insert',[
     check('username','Username is required').not().isEmpty(),
@@ -19,25 +20,57 @@ router.post('/user/insert',[
     if(validationErr.isEmpty()){
     //valid
     
-        const username= req.body.username;//fetch data fromform
+    const username= req.body.username;//fetch data fromform
     const password=req.body.password;//fetch data from form
     const address=req.body.address;
     bcryptjs.hash(password,10,function(err,hash_pw){
         //res.send(hash_pw)
         
     const data=new User({username:username,address:address,password:hash_pw})
-
-
-    data.save();
+    data.save()
+    .then(
+        function(result){
+            res.status(201).json({message:"your registration successful!!"})
+        }
+    )
+    .catch(function(err1){
+        res.status(500).json({message :err1})
     })
-
-    }
+    })
+}
     else{
         //invalid
-        res.send(validationErr.array())
+        res.status(400).json(validationErr.array())
     }
+
 
 
 })
+
+router.get('/user/login',function(req,res){
+    
+    const username= req.body.username;//fetch data fromform
+    const password=req.body.password;//fetch data from form
+    User.findOne({username:username}).
+    then(
+        function(userData){
+            if(userData==null){
+                //your id was not found
+                return res.status(403).json({message:"invalid login"})
+            }
+            //user found
+            bcryptjs.compare(userData.password,password,function(err,result1){
+                if(result1===false){
+                    return res.status(403).json({message:"username or password was not found"})
+                }
+                res.send("correct!!")
+            })
+        }
+
+    )
+    .catch()
+})
+
+
 //update
 module.exports=router;
